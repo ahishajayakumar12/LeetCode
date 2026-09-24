c6ca300b80e2d02661c773ba325712c14ffc7e1d
ahishajayakumar12 <ahishakumar1207@gmail.com>
2026-09-24 13:29:19 +0530

    Time: 17 ms (69.38%), Space: 102.7 MB (17.5%) - LeetHub

---
+++ 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static int gcd(int a,int b){
+        if(b==0)
+            return a;
+        return gcd(b,a%b);
+    }
+    public int minOperations(int[] nums, int[] numsDivide) {
+        int g=numsDivide[0];
+        for(int i=1;i<numsDivide.length;i++){
+            g=gcd(g,numsDivide[i]);
+        }
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(g%nums[i]==0){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
